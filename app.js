c07d7469a652942ca8a1a3aded9787ca10971785
youssef910 <youssef910>
2019-03-21 20:05:56 +0000

    solved part-4 from the project

--- app.js
+++ app.js
@@ -74,8 +74,7 @@ function hoursPassedToday(currentHour, hoursOfDay) {
  * @return {Array<Number>}
  */
 function hoursAheadToday(currentHour, hoursOfDay) {
-  return hoursOfDay.slice(hoursOfDay.indexOf(currentHour));
-  return [];
+  return [hoursOfDay.slice(hoursOfDay.indexOf(currentHour))];
 }
 
 /**
